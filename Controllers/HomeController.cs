a08f9c25112dc52da186c4341dd1bca76930d7b0
CryingCoder <112829574+CryingCoder@users.noreply.github.com>
2022-10-31 22:05:59 -0400

    Send it!

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PoorReddit.Models;
+using System.Diagnostics;
+
+namespace PoorReddit.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        
+        RedditDAL api = new RedditDAL();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ShowPost()
+        {
+            AwwAPI apv = api.GetPosts();
+            /*AllPropsViewModel apv = new AllPropsViewModel();
+            apv.Aww = Aww;
+            
+            List<Child> childItems = api.ConvertChildren();*/
+            return View(apv);
+
+            /*string[] permalink = sp.homeworld.Split("/");
+            int planetId = int.Parse(homeURL[5]);
+            p.World = api.GetPosts(planetId);
+            p.PlanetId = planetId;*/
+
+         }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/RedditDAL.cs
@@ -0,0 +1,18 @@
+﻿using RestSharp;
+using System.Net;
+using System.Text.Json.Serialization;
+
+namespace PoorReddit.Models
+{
+    public class RedditDAL
+    {
+        public AwwAPI GetPosts()
+        {
+            RestClient client = new RestClient($"https://reddit.com/r/awww/.json");
+            RestRequest request = new RestRequest();
+            var response = client.GetAsync<AwwAPI>(request);
+            AwwAPI gp = response.Result;
+            return gp;
+        }
+    }
+}
